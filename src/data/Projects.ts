6a71f29272eb717a65f6bc046dfceb055ca7baf2
Julian Almario <juliandavidalmario@gmail.com>
2025-10-21 22:16:54 -0500

    Header and Project section

---
+++ src/data/Projects.ts
@@ -0,0 +1,30 @@
+import type { Project } from "@/types/types";
+import ticTacToeImage from "@/assets/TicTacToe.jpg";
+import DrivUpImage from "@/assets/DrivUp.png";
+export const projects: Project[] = [{
+    title:"Tic Tac Toe Game",
+    description:"A simple Tic Tac Toe game built with React. It features a user-friendly interface and allows two players to compete against each other.",
+    image:ticTacToeImage.src,
+    linkGit:"https://github.com/JulianAlmario/Tic-Tac-Toe",
+    linkDemo:"https://tic-tac-toe-d19j.onrender.com/"
+},
+{
+    title:"Drivup",
+    description:"DrivUp is a modern web application that facilitates the connection between passengers and drivers for ride-sharing. The platform offers dedicated interfaces for both roles, a real-time messaging system, route visualization through interactive maps, and scheduled trip management — all focused on providing an intuitive and secure user experience.",
+    image:DrivUpImage.src,
+    linkGit:"https://github.com/somilsdiaz/DrivUp",
+    linkDemo:""
+},{
+  title:"Tic Tac Toe Game",
+    description:"A simple Tic Tac Toe game built with React. It features a user-friendly interface and allows two players to compete against each other.",
+    image:ticTacToeImage.src,
+    linkGit:"https://github.com/JulianAlmario/Tic-Tac-Toe",
+    linkDemo:"https://tic-tac-toe-d19j.onrender.com/"
+},
+{
+    title:"Drivup",
+    description:"DrivUp is a modern web application that facilitates the connection between passengers and drivers for ride-sharing. The platform offers dedicated interfaces for both roles, a real-time messaging system, route visualization through interactive maps, and scheduled trip management — all focused on providing an intuitive and secure user experience.",
+    image:DrivUpImage.src,
+    linkGit:"https://github.com/somilsdiaz/DrivUp",
+    linkDemo:""
+}]
